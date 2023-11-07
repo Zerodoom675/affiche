f9755e529a7bc80b7fff273ae6fb9ab6b7719ed8
Zerodoom <zerodoom675@mail.ru>
2023-11-07 17:01:00 +0300

    add test

--- src/test/java/ru/netology/AfficheManagerTest.java
+++ src/test/java/ru/netology/AfficheManagerTest.java
@@ -12,7 +12,6 @@ public class AfficheManagerTest {
         manager.add("Film 1");
         manager.add("Film 2");
         manager.add("Film 3");
-        manager.add("Film 4");
     }
 
     @Test
@@ -36,7 +35,7 @@ public class AfficheManagerTest {
     @Test
     public void shouldInOrderOfAddition() {
 
-        String[] expected = {"Film 1", "Film 2", "Film 3", "Film 4"};
+        String[] expected = {"Film 1", "Film 2", "Film 3"};
         String[] actual = manager.findAll();
 
         Assertions.assertArrayEquals(expected, actual);
@@ -45,7 +44,7 @@ public class AfficheManagerTest {
     @Test
     public void shouldBelowDefaultLimit() {
 
-        String[] expected = {"Film 4", "Film 3", "Film 2", "Film 1"};
+        String[] expected = {"Film 3", "Film 2", "Film 1"};
         String[] actual = manager.findLast();
 
         Assertions.assertArrayEquals(expected, actual);
@@ -54,9 +53,24 @@ public class AfficheManagerTest {
     @Test
     public void shouldEqualToDefaultLimit() {
 
+        manager.add("Film 4");
         manager.add("Film 5");
 
         String[] expected = {"Film 5", "Film 4", "Film 3", "Film 2", "Film 1"};
+        String[] actual = manager.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldAboveTheDefaultLimit2() {
+
+        manager.add("Film 4");
+        manager.add("Film 5");
+        manager.add("Film 6");
+        manager.add("Film 7");
+
+        String[] expected = {"Film 7", "Film 6", "Film 5", "Film 4", "Film 3"};
         String[] actual = manager.findLast();
 
         Assertions.assertArrayEquals(expected, actual);
